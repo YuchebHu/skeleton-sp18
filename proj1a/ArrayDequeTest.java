32c257ca4fcee994ebcfd7da78c1fb04398aa1be
YuchebHu <Yuchebhu@outlook.com>
2023-12-20 23:56:02 +0800

    fix code style and fix linked list deque add first

--- proj1a/ArrayDequeTest.java
+++ proj1a/ArrayDequeTest.java
@@ -8,68 +8,68 @@ import org.junit.Test;
 
 public class ArrayDequeTest {
 
-  @Test
-  public void testaddsizeempty() {
-    ArrayDeque<String> dq = new ArrayDeque<>();
-    boolean isEmpty = dq.isEmpty();
-    assertTrue(dq.isEmpty() == true);
+    @Test
+    public void testaddsizeempty() {
+        ArrayDeque<String> dq = new ArrayDeque<>();
+        boolean isEmpty = dq.isEmpty();
+        assertTrue(dq.isEmpty() == true);
 
-    dq.addFirst("first");
-    assertTrue(1 == dq.size());
+        dq.addFirst("first");
+        assertTrue(1 == dq.size());
 
-    dq.addLast("last");
-    assertTrue(2 == dq.size());
+        dq.addLast("last");
+        assertTrue(2 == dq.size());
 
-    dq.printDeque();
+        dq.printDeque();
 
-    String first = dq.removeFirst();
-    assertTrue("first".equals(first));
+        String first = dq.removeFirst();
+        assertTrue("first".equals(first));
 
-    String last = dq.removeLast();
-    assertTrue("last".equals(last));
+        String last = dq.removeLast();
+        assertTrue("last".equals(last));
 
-    assertTrue(0 == dq.size());
-  }
-
-  // public ArrayDeque<Integer> create(int[] array) {
-  // ArrayDeque<Integer> dq = new ArrayDeque<>();
-  // for (int x : array) {
-  // dq.addLast(x);
-  // }
-  // return dq;
-  // }
-
-  @Test
-  public void testgrowshrink() {
-    ArrayDeque<Integer> dq = new ArrayDeque<>();
-    for (int i = 0; i < 16; i++) {
-      dq.addLast(i);
-    }
-    for (int i = -16; i < 0; i++) {
-      dq.addFirst(i);
-    }
-    {
-      ArrayDeque<Integer> newDq = new ArrayDeque<>(dq);
-      for (int i = 0; i < dq.size(); ++i) {
-        assertTrue(newDq.get(i) == dq.get(i));
-      }
+        assertTrue(0 == dq.size());
     }
 
-    for (int i = 0; i < 16; i++) {
-      assertTrue(-i - 1 == dq.get(i));
-    }
-    for (int i = 0; i < 30; i++) {
-      dq.removeFirst();
-    }
-    assertTrue(2 == dq.size());
-    dq.printDeque();
+    // public ArrayDeque<Integer> create(int[] array) {
+    // ArrayDeque<Integer> dq = new ArrayDeque<>();
+    // for (int x : array) {
+    // dq.addLast(x);
+    // }
+    // return dq;
+    // }
 
-    {
-      ArrayDeque<Integer> newDq = new ArrayDeque<>(dq);
-      for (int i = 0; i < dq.size(); ++i) {
-        assertTrue(newDq.get(i) == dq.get(i));
-      }
+    @Test
+    public void testgrowshrink() {
+        ArrayDeque<Integer> dq = new ArrayDeque<>();
+        for (int i = 0; i < 16; i++) {
+            dq.addLast(i);
+        }
+        for (int i = -16; i < 0; i++) {
+            dq.addFirst(i);
+        }
+        // {
+        // ArrayDeque<Integer> newDq = new ArrayDeque<>(dq);
+        // for (int i = 0; i < dq.size(); ++i) {
+        // assertTrue(newDq.get(i) == dq.get(i));
+        // }
+        // }
+
+        for (int i = 0; i < 16; i++) {
+            assertTrue(-i - 1 == dq.get(i));
+        }
+        for (int i = 0; i < 30; i++) {
+            dq.removeFirst();
+        }
+        assertTrue(2 == dq.size());
+        dq.printDeque();
+
+        // {
+        // ArrayDeque<Integer> newDq = new ArrayDeque<>(dq);
+        // for (int i = 0; i < dq.size(); ++i) {
+        // assertTrue(newDq.get(i) == dq.get(i));
+        // }
+        // }
     }
-  }
 
 }
